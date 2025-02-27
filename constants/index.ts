8bfd23993b1c151e26b4ae52ce57b42e8b95e649
xidstar <xidstar@gmail.com>
2025-02-27 11:04:30 -0500

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,364 @@
+export const manufacturers = [
+  "Acura",
+  "Alfa Romeo",
+  "Aston Martin",
+  "Audi",
+  "Bentley",
+  "BMW",
+  "Buick",
+  "Cadillac",
+  "Chevrolet",
+  "Chrysler",
+  "Citroen",
+  "Dodge",
+  "Ferrari",
+  "Fiat",
+  "Ford",
+  "GMC",
+  "Honda",
+  "Hyundai",
+  "Infiniti",
+  "Jaguar",
+  "Jeep",
+  "Kia",
+  "Lamborghini",
+  "Land Rover",
+  "Lexus",
+  "Lincoln",
+  "Maserati",
+  "Mazda",
+  "McLaren",
+  "Mercedes-Benz",
+  "MINI",
+  "Mitsubishi",
+  "Nissan",
+  "Porsche",
+  "Ram",
+  "Rolls-Royce",
+  "Subaru",
+  "Tesla",
+  "Toyota",
+  "Volkswagen",
+  "Volvo",
+];
+
+export const yearsOfProduction = [
+  { title: "Year", value: "" },
+  { title: "2015", value: "2015" },
+  { title: "2016", value: "2016" },
+  { title: "2017", value: "2017" },
+  { title: "2018", value: "2018" },
+  { title: "2019", value: "2019" },
+  { title: "2020", value: "2020" },
+  { title: "2021", value: "2021" },
+  { title: "2022", value: "2022" },
+  { title: "2023", value: "2023" },
+];
+
+export const fuels = [
+  {
+    title: "Fuel",
+    value: "",
+  },
+  {
+    title: "Gas",
+    value: "Gas",
+  },
+  {
+    title: "Electricity",
+    value: "Electricity",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "About",
+    links: [
+      { title: "How it works", url: "/" },
+      { title: "Featured", url: "/" },
+      { title: "Partnership", url: "/" },
+      { title: "Bussiness Relation", url: "/" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { title: "Zidi Home", url: "/" },
+      { title: "Zidi Real Estate", url: "/" },
+      { title: "Zidi Events", url: "/" },
+      { title: "Zidi Eats", url: "/" },
+    ],
+  },
+  {
+    title: "Socials",
+    links: [
+      { title: "TikTok", url: "/" },
+      { title: "Instagram", url: "/" },
+      { title: "Twitter", url: "/" },
+      { title: "Facebook", url: "/" },
+    ],
+  },
+];
+
+export const carData = [
+  {
+    "make": "Toyota",
+    "model": "Camry",
+    "year": 2020,
+    "color": "Silver",
+    "mileage": 25000,
+    "price": 24999,
+    "mpg": {"city": 29, "highway": 41},
+    "vin": "4T1B11HK0LU200001",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/3/3d/2018_Toyota_Camry_LE_front_6.13.18.jpg"
+    ]
+  },
+  {
+    "make": "Ford",
+    "model": "Mustang",
+    "year": 2019,
+    "color": "Red",
+    "mileage": 45000,
+    "price": 31995,
+    "mpg": {"city": 16, "highway": 25},
+    "vin": "1FA6P8CF2K5200002",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/1/1d/2019_Ford_Mustang_5.0_Front.jpg"
+    ]
+  },
+  {
+    "make": "Honda",
+    "model": "Civic",
+    "year": 2021,
+    "color": "Blue",
+    "mileage": 15000,
+    "price": 22999,
+    "mpg": {"city": 32, "highway": 42},
+    "vin": "2HGFC2F56MH600003",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/8/87/Honda_Civic_%282021%29_sedan_Sport_DSC_7057.jpg"
+    ]
+  },
+  {
+    "make": "Tesla",
+    "model": "Model 3",
+    "year": 2022,
+    "color": "White",
+    "mileage": 10000,
+    "price": 44990,
+    "mpg": {"equivalent": {"city": 134, "highway": 122}},
+    "vin": "5YJ3E1EB0NF000004",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/9/91/2019_Tesla_Model_3_Performance_AWD_Front.jpg"
+    ]
+  },
+  {
+    "make": "Chevrolet",
+    "model": "Silverado",
+    "year": 2018,
+    "color": "Black",
+    "mileage": 75000,
+    "price": 27995,
+    "mpg": {"city": 15, "highway": 20},
+    "vin": "3GCUKREC5JG300005",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/f/fc/Chevrolet_Silverado_2016-2018_Crew_Cab_Z71.jpeg"
+    ]
+  },
+  {
+    "make": "BMW",
+    "model": "3 Series",
+    "year": 2022,
+    "color": "Gray",
+    "mileage": 12000,
+    "price": 42900,
+    "mpg": {"city": 26, "highway": 36},
+    "vin": "WBA5J7C05N6D00006",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/e/e7/BMW_3_SERIES_LWB_SEDAN_%28G20%29_China_%2828%29.jpg"
+    ]
+  },
+  {
+    "make": "Mercedes-Benz",
+    "model": "C-Class",
+    "year": 2021,
+    "color": "Black",
+    "mileage": 18000,
+    "price": 41999,
+    "mpg": {"city": 24, "highway": 35},
+    "vin": "WDDWF8CB7MA000007",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/2/24/Mercedes-Benz_W206_IMG_5796.jpg"
+    ]
+  },
+  {
+    "make": "Audi",
+    "model": "A4",
+    "year": 2020,
+    "color": "Blue",
+    "mileage": 22000,
+    "price": 34950,
+    "mpg": {"city": 27, "highway": 34},
+    "vin": "WAUBNBF40LA000008",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/7/78/2020_Audi_A4_S_Line_35_TDi_S-A_facelift_2.0_Front.jpg"
+    ]
+  },
+  {
+    "make": "Nissan",
+    "model": "Rogue",
+    "year": 2021,
+    "color": "Silver",
+    "mileage": 19000,
+    "price": 26999,
+    "mpg": {"city": 27, "highway": 35},
+    "vin": "5N1AT3BA8MC000009",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/3/36/2016_Nissan_Rogue%2C_Front_Left%2C_04-13-2021.jpg"
+    ]
+  },
+  {
+    "make": "Hyundai",
+    "model": "Elantra",
+    "year": 2023,
+    "color": "Red",
+    "mileage": 5000,
+    "price": 23999,
+    "mpg": {"city": 33, "highway": 42},
+    "vin": "KMHL34DG3PU000010",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/2/27/HYUNDAI_ELANTRA_CN7_China_%2820%29.jpg"
+    ]
+  },
+  {
+    "make": "Jeep",
+    "model": "Wrangler",
+    "year": 2019,
+    "color": "Green",
+    "mileage": 35000,
+    "price": 35900,
+    "mpg": {"city": 17, "highway": 23},
+    "vin": "1C4HJXEN2KW000011",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/1/12/Jeep_Wrangler_Mopar_Genf_2019_1Y7A5060.jpg"
+    ]
+  },
+  {
+    "make": "Subaru",
+    "model": "Outback",
+    "year": 2022,
+    "color": "Silver",
+    "mileage": 8000,
+    "price": 31950,
+    "mpg": {"city": 26, "highway": 33},
+    "vin": "4S4BTAMC9N3000012",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/4/4f/2022_Subaru_Outback_Wilderness_%28United_States%29_front_view_01.png"
+    ]
+  },
+  {
+    "make": "Kia",
+    "model": "Sportage",
+    "year": 2020,
+    "color": "White",
+    "mileage": 28000,
+    "price": 21999,
+    "mpg": {"city": 23, "highway": 30},
+    "vin": "KNDPMCAC5L7000013",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/2/22/2020_Kia_Sportage_4_CRDi_ISG_MHEV.jpg"
+    ]
+  },
+  {
+    "make": "Volkswagen",
+    "model": "Golf",
+    "year": 2018,
+    "color": "Red",
+    "mileage": 42000,
+    "price": 18900,
+    "mpg": {"city": 29, "highway": 37},
+    "vin": "3VW2K7AJ7JM000014",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/1/1b/VW_Golf_R_Oettinger%2C_Berlin_%281X7A3947%29.jpg"
+    ]
+  },
+  {
+    "make": "Porsche",
+    "model": "911 GT3",
+    "year": 2023,
+    "color": "Silver",
+    "mileage": 1500,
+    "price": 124999,
+    "mpg": {"city": 18, "highway": 24},
+    "vin": "WP0AB2A97PS000015",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/1/18/Porsche_911_GT3_4.0_%28992%29_1X7A7256.jpg"
+    ]
+  },
+  {
+    "make": "Lexus",
+    "model": "RX",
+    "year": 2021,
+    "color": "Black",
+    "mileage": 16000,
+    "price": 47900,
+    "mpg": {"city": 20, "highway": 27},
+    "vin": "2T2BZMDA9MC000016",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/4/4b/Lexus_RX_AL20_Shishi_01_2021-12-24.jpg"
+    ]
+  },
+  {
+    "make": "Ford",
+    "model": "F-150",
+    "year": 2021,
+    "color": "White",
+    "mileage": 22000,
+    "price": 42999,
+    "mpg": {"city": 20, "highway": 24},
+    "vin": "1FTFW1E53MFA00017",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/d/d9/Ford_F-150_P702_white_%281%29.jpg"
+    ]
+  },
+  {
+    "make": "Tesla",
+    "model": "Model S",
+    "year": 2022,
+    "color": "Red",
+    "mileage": 9000,
+    "price": 94990,
+    "mpg": {"equivalent": {"city": 120, "highway": 112}},
+    "vin": "5YJSA1E28NF000018",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/9/9a/Tesla_Model_S_P85D_%2816467342602%29.jpg"
+    ]
+  },
+  {
+    "make": "Toyota",
+    "model": "RAV4",
+    "year": 2021,
+    "color": "Black",
+    "mileage": 18000,
+    "price": 27950,
+    "mpg": {"city": 28, "highway": 35},
+    "vin": "JTMFB3FV7MD000019",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/5/5d/Toyota_Rav4_2020_Advance_Plus_4x4.jpg"
+    ]
+  },
+  {
+    "make": "Honda",
+    "model": "Accord",
+    "year": 2020,
+    "color": "White",
+    "mileage": 24000,
+    "price": 24900,
+    "mpg": {"city": 30, "highway": 38},
+    "vin": "1HGCV1F14LA000020",
+    "pictures": [
+      "https://upload.wikimedia.org/wikipedia/commons/6/6b/2020_Honda_Accord_1.5_TC-P_98.jpg"
+    ]
+  }
+]
